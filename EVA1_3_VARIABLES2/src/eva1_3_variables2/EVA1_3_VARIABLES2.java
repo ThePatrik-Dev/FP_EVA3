c7d9c7b488f9b6b488635cd7f2da8da01ea1fe46
Joel Alderete <thej14653@gmail.com>
2024-08-28 20:14:16 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES2/src/eva1_3_variables2/EVA1_3_VARIABLES2.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_variables2;
+
+/**
+ *
+ * @author elmas
+ */
+public class EVA1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+            String Marca;
+            String Modelo;
+            int Año;
+            double Precio;
+            
+            //Asignacion
+            
+            Marca = "Mazda";
+            Modelo = "Limux";
+            Año = 2021;
+            Precio = 200231.31;
+            
+            System.out.println(Marca);
+            System.out.println(Modelo);
+            System.out.println(Año);
+            System.out.println(Precio);
+            
+            //Alderete Aguirre Diego Joel
+
+            
+        
+    }
+    
+}
